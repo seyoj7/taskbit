b1db171b71df8f9ca799b06169d311b6d7888a45
seyoj7 <joyeshpaul55@gmail.com>
2026-09-18 22:47:21 +0530

    chore: remove unused file

--- contract/ignition/modules/TaskEscrow.ts
+++ contract/ignition/modules/TaskEscrow.ts
@@ -6,8 +6,6 @@ const TaskEscrowModule = buildModule("TaskEscrowModule", (m) => {
   // We define a parameter that defaults to the environment variable or the zero address
   const usdc = m.getParameter("usdc", USDC_ADDRESS);
 
-
-
   // Deploy the TaskEscrow contract with the USDC address as the constructor argument
   const taskEscrow = m.contract("TaskEscrow", [usdc]);
 
